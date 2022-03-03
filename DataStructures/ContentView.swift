4af6c9f0524c85a0338999d539b388b83b2da72f
Harry Sibson <Sibson.h@etoncollege.org.uk>
2022-03-03 11:51:36 +0000

    Initial Commit

---
+++ DataStructures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DataStructures
+//
+//  Created by Sibson, Harry (AGDF) on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataStructures/DataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructuresApp.swift
+//  DataStructures
+//
+//  Created by Sibson, Harry (AGDF) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
